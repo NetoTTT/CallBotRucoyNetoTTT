65119f653121cafc6aeb488dc14261a208d04cb3
NetoTTT <142108410+NetoTTT@users.noreply.github.com>
2024-09-29 19:02:18 -0300

    Update de Correção 5.0

--- index.js
+++ index.js
@@ -58,6 +58,10 @@ async function createCallBossIdChannel(guild) {
                     {
                         id: '1290057620391985224', // Coloque o ID do cargo de administradores
                         allow: [PermissionsBitField.Flags.ViewChannel, PermissionsBitField.Flags.SendMessages], // Permite que os administradores vejam e enviem mensagens
+                    },
+                    {
+                        id: '1180272578141622302', // Coloque o ID do cargo de administradores
+                        allow: [PermissionsBitField.Flags.ViewChannel, PermissionsBitField.Flags.SendMessages], // Permite que os bots vejam e enviem mensagens
                     }
                 ]
             });
